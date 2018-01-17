95b71a713cf4b46405a51c7c9af4a32c4c4b31b7
Maxime Blanc <max@jellycat.fr>
2018-01-17 18:15:41 +0100

    refacto update.php

--- update.php
+++ update.php
@@ -5,8 +5,38 @@ session_start();
 // Get the task ID
 $id = $_GET['id'];
 
-// If the edit form has been posted
-if($_POST['update'] === 'yes') {
+if(isset($_POST)) {
+
+  // If the edit form has been posted
+  if($_POST['update'] === 'yes' && isset($_POST['title'])) {
+    modifyTask();
+  }
+  // If the add form has been posted
+  else {
+    // Add the new task to $_SESSION
+    if(isset($_POST['newTitle']) && isset($_POST['category']))
+    array_push($_SESSION['tasks'], addTask($_POST['newTitle'], $_POST['category']));
+  }
+
+}
+
+// Get back to index.php afterwards
+header('Location:index.php');
+
+function addTask($newTitle, $cat) {
+  // Create a new task array with posted values
+  $newTask = [
+    // Increment index based on $_SESSION['tasks'] length
+    'id' => count($_SESSION['tasks']) + 1,
+    'title' => $newTitle,
+    'category' => strtolower($cat),
+    'done' => false
+  ];
+  return $newTask;
+}
+
+function modifyTask() {
+  global $id;
   foreach($_SESSION['tasks'] as $index => $task) {
     // Check if the ID exists
     if($task['id'] == $id) {
@@ -15,25 +45,4 @@ if($_POST['update'] === 'yes') {
     }
   }
 }
-// If the add form has been posted
-else {
-  // Add the new task to $_SESSION
-  array_push($_SESSION['tasks'], addTask());
-}
-
-// Get back to index.php afterwards
-header('Location:index.php');
-
-function addTask() {
-  // Create a new task array with posted values
-  $newTask = [
-    // Increment index based on $_SESSION['tasks'] length
-    'id' => count($_SESSION['tasks']) + 1,
-    'title' => $_POST['newTitle'],
-    'category' => strtolower($_POST['category']),
-    'done' => false
-  ];
-  return $newTask;
-}
-
 ?>
